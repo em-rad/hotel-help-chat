6145a3ea639d86b4bb4d5439b24c1fed6162502f
Emily <em.radocha@gmail.com>
2020-11-21 14:48:40 -0500

    Hotel Page Startup

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+	<div id="app">
+		<div id="nav">
+			<img src="../public/bird-bath.png" />
+			<router-link to="/">Home |</router-link>
+			<router-link to="/"> Reservations |</router-link>
+			<router-link to="/"> Deals |</router-link>
+			<router-link to="/"> Contact</router-link>
+		</div>
+		<router-view />
+	</div>
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: 'Open Sans', sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
 }
 
 #nav {
-  padding: 30px;
+	padding: 30px;
+	border-bottom: 8px double #b8a57f;
+	width: 101%;
+	a {
+		font-weight: bold;
+		color: #2c3e50;
+		text-decoration: none;
+		font-size: 18pt;
+		word-spacing: 10px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+		&.router-link-exact-active {
+			color: #b8a57f;
+		}
+	}
+	img {
+		max-width: 85px;
+		position: absolute;
+		left: 30px;
+		top: 12px;
+	}
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,179 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+	<div class="home">
+		<h1>The Bird Bath</h1>
+		<h2>Hang with the birds</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla luctus lacus sem, eget semper tortor pellentesque non. In vitae volutpat neque. Etiam condimentum laoreet lacus non laoreet. Etiam
+			sagittis gravida lacus in imperdiet. Ut nulla nisi, imperdiet et lobortis ut, vestibulum et tortor. Integer ac euismod nulla. Suspendisse posuere diam eget erat sagittis, ut tincidunt mi
+			ultrices. Vestibulum ut tortor nibh. Nam ultricies orci vitae lobortis sagittis.
+		</p>
+		<img class="bird-hotel" src="../..//public/hotel.jpg" />
+		<p>
+			Proin facilisis nisl eu lacus maximus, eu facilisis leo aliquet. Cras mollis laoreet diam, nec euismod enim facilisis in. Nullam feugiat gravida arcu quis dignissim. Vivamus sed dapibus lorem.
+			Curabitur nec dolor non quam consectetur sagittis ac sed ex. Donec vitae orci enim. In ut vulputate risus. Integer pretium eros turpis, sit amet malesuada dolor tincidunt at. Nullam porta lacus
+			et nibh venenatis, vel consequat metus porttitor. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc libero lacus, condimentum nec lacinia a, blandit ut lorem.
+		</p>
+		<div class="image-container">
+			<img class="examples" src="../..//public/room1.jpg" />
+			<img class="examples" src="../..//public/room2.jpg" />
+			<img class="examples" src="../..//public/bath.jpg" />
+		</div>
+		<button class="book-now">BOOK NOW</button>
+
+		<button class="open-button" @click="openForm()">Chat</button>
+
+		<div class="chat-popup" id="myForm">
+			<form class="form-container">
+				<h3>Customer Service Chat</h3>
+
+				<label for="msg"><b>Message</b></label>
+				<!-- TODO when message is sent, create new element and add child into message div -->
+				<textarea placeholder="Type message..." name="msg" required></textarea>
+				<!-- TODO change message button to be handled by 'Enter' -->
+				<button type="submit" class="btn">Send</button>
+				<!-- TODO change cancel button to X in top right -->
+				<button type="button" class="btn cancel" @click="closeForm()">Close</button>
+			</form>
+		</div>
+		<footer></footer>
+	</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator';
 
-@Component({
-  components: {
-    HelloWorld
-  }
-})
-export default class Home extends Vue {}
+export default {
+	methods: {
+		openForm() {
+			const form = document.querySelector('#myForm') as HTMLElement;
+			form.style.display = 'block';
+		},
+		closeForm() {
+			const form = document.querySelector('#myForm') as HTMLElement;
+			form.style.display = 'none';
+		},
+	},
+};
 </script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+h1 {
+	padding-top: 1em;
+	font-size: 72pt;
+	color: #0b2027;
+	font-family: 'Dancing Script', cursive;
+	margin: 0px;
+}
+h2 {
+	font-size: 48pt;
+	color: #0b2027;
+	font-family: 'Dancing Script', cursive;
+	margin: 0 0 1em 0;
+}
+p {
+	margin: 0 2em 3em 2em;
+	font-size: 16pt;
+}
+.bird-hotel {
+	margin: auto auto;
+	border: 10px solid #957d95;
+	margin-bottom: 2em;
+}
+.book-now {
+	font-size: 18pt;
+	background-color: #40798c;
+	color: white;
+	font-family: 'Open Sans', sans-serif;
+	border: none;
+	margin: 2em auto 0 auto;
+	&::hover {
+		background-color: #957d95;
+	}
+}
+.home {
+	background-image: url('../../public/foggy_birds.png');
+}
+.image-container {
+	display: flex;
+	justify-content: space-between;
+	margin: 0 40px 0 40px;
+}
+.examples {
+	width: 400px;
+	height: 400px;
+	border: 10px solid #957d95;
+}
+
+/* Button used to open the chat form - fixed at the bottom of the page */
+.open-button {
+	background-color: #555;
+	color: white;
+	padding: 16px 20px;
+	border: none;
+	cursor: pointer;
+	opacity: 0.8;
+	position: fixed;
+	bottom: 23px;
+	right: 28px;
+	width: 280px;
+}
+
+/* The popup chat - hidden by default */
+.chat-popup {
+	display: none;
+	position: fixed;
+	bottom: 0;
+	right: 15px;
+	border: 3px solid #f1f1f1;
+	z-index: 9;
+}
+
+/* Add styles to the form container */
+.form-container {
+	max-width: 300px;
+	padding: 10px;
+	background-color: white;
+}
+
+/* Full-width textarea */
+.form-container textarea {
+	width: 100%;
+	padding: 15px;
+	margin: 5px 0 22px 0;
+	border: none;
+	background: #f1f1f1;
+	resize: none;
+	min-height: 200px;
+}
+
+/* When the textarea gets focus, do something */
+.form-container textarea:focus {
+	background-color: #ddd;
+	outline: none;
+}
+
+/* Set a style for the submit/send button */
+.form-container .btn {
+	background-color: #4caf50;
+	color: white;
+	padding: 16px 20px;
+	border: none;
+	cursor: pointer;
+	width: 100%;
+	margin-bottom: 10px;
+	opacity: 0.8;
+}
+
+/* Add a red background color to the cancel button */
+.form-container .cancel {
+	background-color: red;
+}
+
+/* Add some hover effects to buttons */
+.form-container .btn:hover,
+.open-button:hover {
+	opacity: 1;
+}
+</style>
